27702181d4b9a50312e2749fa15941a1ffb86767
jackAinsworth <136799559+jackAinsworth@users.noreply.github.com>
2023-12-16 15:34:24 +0100

    init

---
+++ Server/server.js
@@ -0,0 +1,38 @@
+const io = require('socket.io')(3001, {
+    cors: {
+        origin:['http://localhost:3000'],
+    },
+})
+
+let userAnzahl = 0;
+
+
+io.on("connection", socket => {
+    console.log(socket.id);
+
+    socket.on("sendToken", (room, row,col)=>{
+        console.log("triggered");
+
+        socket.to(room).emit("receiveToken", row, col);
+        //socket.broadcast.emit("receiveToken", row, col);
+    })
+
+
+
+
+
+
+
+    socket.on("joinRoom", (room)=> {
+            socket.join(room);
+            userAnzahl = io.sockets.adapter.rooms.get(room).size;
+            console.log("user joined room "+ room+ "  position: "+ userAnzahl);
+            socket.emit("informTurn", userAnzahl);
+            // if room.size > 2 --> emit(staylockedlock) else --> join room inform user  
+            
+    })
+    
+
+
+})
+
